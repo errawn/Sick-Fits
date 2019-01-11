930dc8b5731d06f360f327ac84f78cc371324838
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-11 13:53:19 +0800

    connected react to apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,24 @@
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
 
 import Layout from '../components/Layout'
 
 class MyApp extends App {
     render() {
-        const { Component } = this.props
+        const { Component, apollo } = this.props
         
         return (
             <Container>
-                <Layout>
-                    <h1>Im on every page</h1>
-                    <Component />
-                </Layout>
+                <ApolloProvider client={apollo}>
+                    <Layout>
+                        <h1>Im on every page</h1>
+                        <Component />
+                    </Layout>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp
+export default withData(MyApp) // withData HOC make the appolo-client accessible to this.props
