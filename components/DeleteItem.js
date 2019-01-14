55db68b0299d3935632f4ed7977bf114f028a7d7
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-14 14:54:56 +0800

    update cache after delete

--- components/DeleteItem.js
+++ components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
     mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -11,11 +12,27 @@ const DELETE_ITEM_MUTATION = gql`
 `
 
 class DeleteItem extends Component {
+    // 2 options after deleting items:
+    // 1. refetch the items via query (will create an http call over server)
+    // 2. manually update the cache to remove the deleted item
+
+    update = (cache, payload) => { // payload is the data came back from the item that got deleted
+        // manually update the cache on the client
+        // read the cache for the items
+        const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
+        console.log(data, payload)
+        // filter the deleted item out of the page
+        data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id)
+        // put the items back
+        cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+    }
+
     render() {
         return (
             <Mutation
                 mutation={DELETE_ITEM_MUTATION}
                 variables={{ id: this.props.id }}
+                update={this.update}
             >
                 {(deleteItem, { error }) => (
                     <button onClick={() => {
