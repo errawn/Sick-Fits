694abd6626f13183eaf5b96e685121d3e5af7e99
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-20 22:05:51 +0800

    Display name in nav if signed in

--- components/Nav.js
+++ components/Nav.js
@@ -1,24 +1,31 @@
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
+import User from './User'
 
 const Nav = () => (
-    <NavStyles>
-        <Link href='/'>
-            <a>Shop</a>
-        </Link>
-        <Link href='/sell'>
-            <a>Sell</a>
-        </Link>
-        <Link href='/signup'>
-            <a>Signup</a>
-        </Link>
-        <Link href='/orders'>
-            <a>Orders</a>
-        </Link>
-        <Link href='/me'>
-            <a>Me</a>
-        </Link>
-    </NavStyles>
+  <NavStyles>
+      <User>
+        {({ data: { currentUser } }) => {
+          if (currentUser) return <p>{currentUser.name}</p>
+          return null
+        }}
+      </User>
+      <Link href='/'>
+          <a>Shop</a>
+      </Link>
+      <Link href='/sell'>
+          <a>Sell</a>
+      </Link>
+      <Link href='/signup'>
+          <a>Signup</a>
+      </Link>
+      <Link href='/orders'>
+          <a>Orders</a>
+      </Link>
+      <Link href='/me'>
+          <a>Me</a>
+      </Link>
+  </NavStyles>
 )
 
 export default Nav
